a7f643fd59a776f4ed2b8987fc9f416f82c9eddb
flyingabove <flyingabove@gmail.com>
2017-06-11 08:46:27 -0700

    test

--- Hw4-NeuralNetworks.R
+++ Hw4-NeuralNetworks.R
@@ -3,15 +3,16 @@ library(magrittr)
 
 ###Cleaning###
 sentiment_df<-fread("data/sentiment_raw.csv")
+sentiment_df<-sentiment_df[1:10000]
 sentiment_df$ascii<-iconv(sentiment_df$JustText, "latin1", "ASCII", sub="")
 sentiment_df$no_repeats<-gsub("([[:alpha:]])\\1{2,}", "\\1", sentiment_df$ascii)
 sentiment_df$no_repeats<-gsub("(?<=[\\s])\\s*|^\\s+|\\s+$","",perl=TRUE,sentiment_df$no_repeats)
 sentiment_df<-sentiment_df[sapply(gregexpr("\\W+", sentiment_df$no_repeats), length) >1,]
 
-sentiment_df_small<-sentiment_df[1:10000,]
-writeLines(sentiment_df_small$no_repeats,"smalltest.txt")
-
-factor_list<-factor(readLines("wordList.txt"))
+sentiment_df_small<-sentiment_df
+writeLines(sentiment_df_small$no_repeats,"data/smalltest.txt")
+system("grep -o -E '\\w+' smalltest.txt | sort -u -f > wordlist.txt")
+factor_list<-factor(readLines("data/wordList.txt"))
 text_raw_list<-strsplit(sentiment_df_small$no_repeats,split = " ")
 
 get_training<-function(string_in,levels){
@@ -40,14 +41,14 @@ maxlen <- 80  # cut texts after this number of words
 batch_size <- 32
 
 print('Loading data...\n')
-print(length(x_train), 'train sequences\n')
-print(length(x_test), 'test sequences\n')
+cat(length(x_train), 'train sequences\n')
+cat(length(x_test), 'test sequences\n')
 
 print('Pad sequences (samples x time)\n')
 x_train <- pad_sequences(x_train, maxlen = maxlen)
 x_test <- pad_sequences(x_test, maxlen = maxlen)
-print('x_train shape:', dim(x_train), '\n')
-print('x_test shape:', dim(x_test), '\n')
+cat('x_train shape:', dim(x_train), '\n')
+cat('x_test shape:', dim(x_test), '\n')
 
 print('Build model...\n')
 model <- keras_model_sequential()
@@ -56,7 +57,7 @@ model %>%
   layer_lstm(units = 64, dropout = 0.2, recurrent_dropout = 0.2) %>% 
   layer_dense(units = 1, activation = 'sigmoid')
 
-# try using different optimizers and different optimizer configs
+#Model Settings
 model %>% compile(
   loss = 'binary_crossentropy',
   optimizer = 'adam',
@@ -67,13 +68,12 @@ print('Train...\n')
 model %>% fit(
   x_train, y_train,
   batch_size = batch_size,
-  epochs = 1,
+  epochs = 10,
   validation_data = list(x_test, y_test)
 )
 scores <- model %>% evaluate(
   x_test, y_test,
   batch_size = batch_size
 )
-
 cat('Test score:', scores[[1]])
 cat('Test accuracy', scores[[2]])
